840852c7d246239386c03c3bedc6fa3473ece7d3
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-03-18 05:45:36 +0300

    Refactor problem 3

--- Problem3/Solution.py
+++ Problem3/Solution.py
@@ -1,10 +1,12 @@
 class Solution:
 
-    # Algorithm's time complexity is O(n) with sliding window technique.
-    #
-    # LeetCode:
-    # - Runtime: 76 ms, faster than 89.92% of Python3 online submissions for Longest Substring Without Repeating Characters.
-    # - Memory Usage: 13.2 MB, less than 5.31% of Python3 online submissions for Longest Substring Without Repeating Characters.
+    """
+    Algorithm's time complexity is O(n) with sliding window technique.
+
+    LeetCode:
+     - Runtime: 76 ms, faster than 89.92% of Python3 online submissions for Longest Substring Without Repeating Characters.
+     - Memory Usage: 13.2 MB, less than 5.31% of Python3 online submissions for Longest Substring Without Repeating Characters.
+    """
 
     def lengthOfLongestSubstring(self, s):
 
@@ -13,19 +15,19 @@ class Solution:
         :rtype: int
         """
 
-        hashmap = dict()
+        hashtable = dict()
         l = len(s)
 
         # [i,j), sliding window indexes
         i, j, result = 0, 0, 0
 
         while j < l:
-            if s[j] in hashmap and i < hashmap[s[j]] + 1:
+            if s[j] in hashtable and i < hashtable[s[j]] + 1:
                 # slide i
-                i = hashmap[s[j]] + 1
+                i = hashtable[s[j]] + 1
 
             # override
-            hashmap[s[j]] = j
+            hashtable[s[j]] = j
             j += 1
 
             # update result
