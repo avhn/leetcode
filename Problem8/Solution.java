16929d78f34eba4b0aa4ad03cc0490de576f7a06
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-03-24 01:12:09 +0300

    Update problem 8

--- Problem8/Solution.java
+++ Problem8/Solution.java
@@ -1,45 +1,39 @@
-import java.lang.Character;
-
-
 class Solution {
 
     /**
      * Time complexity O(n).
-     * Runtime: 18 ms, faster than 58.59% of Java online submissions for String to Integer (atoi).
-     * Memory Usage: 38.6 MB, less than 14.00% of Java online submissions for String to Integer (atoi).
+     * Runtime: 14 ms, faster than 99.86% of Java online submissions for String to Integer (atoi).
+     * Memory Usage: 38.4 MB, less than 22.66% of Java online submissions for String to Integer (atoi).
      *
      * @param str String to convert to Integer
      * @return Integer value, between -2**31 <= value <= 2**31. value == 0 mostly means conversion unsuccessful.
      */
     public int myAtoi(String str) {
-        str = str.trim();
-        if (str.length() == 0) return 0;
-        String strnum = "";
-        Character c = str.charAt(0);
+        double result = 0;
+        byte sign = 1;
 
-        // check if it's a number
-        if (Character.isDigit(c)) strnum += c;
-        else {
-            if (c == '-' || c == '+') {
-                if (1 < str.length() && Character.isDigit(str.charAt(1))) strnum += c;
-                else return 0;
-            } else return 0;
+        char[] array = str.trim().toCharArray();
+        boolean firstIter = true;
+        for (char c : array) {
+            if ('0' <= c && c <= '9') {
+                result *= 10;
+                result += c - '0';
+            } else if (firstIter && c == '-') {
+                sign = -1;
+            } else if (firstIter && c == '+') {
+                ;
+            } else {
+                break;
+            }
+
+            if (firstIter) firstIter = false;
         }
 
-        // add the whole number
-        for (int i = 1; i < str.length(); i++) {
-            c = str.charAt(i);
+        if (result <= Integer.MAX_VALUE) {
+            return (int)(sign * result);
 
-            if (Character.isDigit(c)) strnum += c;
-            else break;
-        }
-
-        // convert and return
-        try {
-            return Integer.parseInt(strnum);
-
-        } catch (NumberFormatException e) {
-            if (strnum.charAt(0) == '-') return Integer.MIN_VALUE;
+        } else {
+            if (sign < 0) return Integer.MIN_VALUE;
             else return Integer.MAX_VALUE;
         }
     }
