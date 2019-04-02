ecc7a8cba61602c646cb1f61ff1bc8181f00f202
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-04-02 23:41:21 +0300

    Add problem 15

---
+++ Problem15/README.md
@@ -0,0 +1,20 @@
+## 15. 3Sum
+[![Level](https://img.shields.io/badge/-Medium-important.svg)](https://github.com/Anaxilaus/LeetCode/tree/master/Problem15)
+
+
+Given an array `nums` of *n* integers, are there elements *a, b, c* in `nums` such that *a + b + c* = 0? Find all unique triplets in the array which gives the sum of zero.
+
+**Note:**
+
+The solution set must not contain duplicate triplets.
+
+**Example:**
+```
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+```
---
+++ Problem15/Solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+class Solution {
+
+    /**
+     * Time Complexity: O(n^2).
+     * Runtime: 30 ms, faster than 97.65% of Java online submissions for 3Sum.
+     * Memory Usage: 48.1 MB, less than 85.11% of Java online submissions for 3Sum.
+     *
+     * @param nums array of integers to examine
+     * @return list of lists contains three decimal numbers and no dublicates
+     * @see java.util.List
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new LinkedList<>();
+        int i, li, ri, sum; // li = left index, ri = left index
+
+        for (i = 0; i < nums.length - 2; i++) {
+            // skip dublicates after the first one
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                li = i + 1;
+                ri = nums.length - 1;
+
+                while (li < ri) {
+                    sum = nums[i] + nums[li] + nums[ri];
+
+                    if (sum == 0) {
+                        result.add(Arrays.asList(nums[i], nums[li], nums[ri]));
+                        while (li < ri && nums[li] == nums[li + 1]) li++; // skip dublicates
+                        li++;
+                    }
+                    else if (sum < 0) li++;
+                    else ri--;
+                }
+            }
+        }
+        return result;
+
+    }
+}
