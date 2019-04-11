c37946ef18880f221e1c88b20c6729aba2b31d51
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-04-11 18:01:13 +0300

    Add problem 17

---
+++ Problem17/Solution.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    char[][] buttonMap = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'},
+            {'g', 'h', 'i'},
+            {'j', 'k', 'l'},
+            {'m', 'n', 'o'},
+            {'p', 'q', 'r', 's'},
+            {'t', 'u', 'v'},
+            {'w', 'x', 'y', 'z'}
+    };
+
+    /**
+     * Time Complexity: O(4^n), where n is length of digits.
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Letter Combinations of a Phone Number.
+     * Memory Usage: 37 MB, less than 89.31% of Java online submissions for Letter Combinations of a Phone Number.
+     *
+     * @param digits digits of buttons
+     * @return combinations as String values
+     * @see List
+     */
+    public List<String> letterCombinations(String digits) {
+        ArrayList<String> result = new ArrayList<>();
+
+        char[] charArray = digits.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            int button = charArray[i] - '2';
+
+            if (i == 0) {
+                // initialization
+                for (int j = 0; j < buttonMap[button].length; j++) {
+                    result.add(String.valueOf(buttonMap[button][j]));
+                }
+            } else {
+                // cartesian multiplication
+                ArrayList<String> newResult = new ArrayList<>();
+                for (int r = 0; r < result.size(); r++) {
+                    for (int j = 0; j < buttonMap[button].length; j++) {
+                        String currChar = String.valueOf(buttonMap[button][j]);
+                        newResult.add(result.get(r) + currChar);
+                    }
+                }
+                result = newResult;
+            }
+        }
+
+        return result;
+    }
+}
