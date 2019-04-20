9201e3a0f39975410df993e3825f2674e619272c
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-04-20 16:30:31 +0300

    Add problem 22

---
+++ Problem22/README.md
@@ -0,0 +1,15 @@
+# 22. Generate Parentheses
+[![Medium](https://img.shields.io/badge/-Medium-important.svg)](https://github.com/Anaxilaus/LeetCode/tree/master/Problem22)
+
+Given *n* pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given *n* = 3, a solution set is:
+```
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+```
---
+++ Problem22/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+class Solution {
+
+    /**
+     * Time Complexity: O(4^n / sqrt(n)), dataset increases in sync with Catalan Numbers.
+     * Runtime: 1 ms, faster than 97.31% of Java online submissions for Generate Parentheses.
+     * Memory Usage: 37.3 MB, less than 97.23% of Java online submissions for Generate Parentheses.
+     *
+     * Using DP approach:
+     * DP[n] = [for j = 0 to n do ['(' + DP[j] + ')'] * DP[n - j - 1]]
+     *
+     * @param n number of parenthesizations
+     * @return list of Strings contains legit combinations
+     * @see List
+     */
+    public List<String> generateParenthesis(int n) {
+        // bottom up approach with memoization
+        List<List<String>> DP = new ArrayList<>(n + 1);
+        for (int i = 0; i < n + 1; i++) DP.add(new ArrayList<>());
+
+        DP.get(0).add("");
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                // x = DP[j], y = DP[i - j - 1]
+                // parenthesizing s, therefore -1 for index
+                for (String x : DP.get(j)) {
+                    for (String y : DP.get(i - j - 1)) {
+                        DP.get(i).add("(" + x + ")" + y);
+                    }
+                }
+            }
+        }
+        return DP.get(n);
+    }
+}
