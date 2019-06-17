da1c97d94f3e9830421d76a4ffa28e66b58364cc
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-06-17 04:59:56 +0300

    Add problem 4
    Squashed commit of the following:
    
    commit d40ec133010f7f26747b1fb45b34aacfc7f9c229
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Mon Jun 17 04:55:03 2019 +0300
    
        Complete problem
    
    commit 40e3b3ce175cff077c10781aeaf9532c17706c5f
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Sun Jun 16 00:29:52 2019 +0300
    
        Add template

---
+++ Problem4/Solution.java
@@ -0,0 +1,62 @@
+class Solution {
+
+    /**
+     * Binary search smaller array to find median.
+     *
+     * Time Complexity: O(log(min(A, B))).
+     *
+     * @param A array
+     * @param B array
+     * @return median
+     */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+
+        // make B bigger than A
+        if (A.length > B.length) {
+            return findMedianSortedArrays(B, A);
+        }
+
+        int m = A.length;
+        int n = B.length;
+        int totalHalfLength = (m + n + 1) / 2;
+
+        int i, j;
+        int iMin = 0;
+        int iMax = m;
+        while (iMin <= iMax) {
+            i = (iMin + iMax) / 2;
+            j = totalHalfLength - i;
+
+            if (i > iMin && A[i - 1] > B[j]) {  // too big
+                iMax = i - 1;
+            } else if (i < iMax && A[i] < B[j - 1]) {  // too small
+                iMin = i + 1;
+            } else {  // found position
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+
+                if ((m + n) % 2 != 0) { // odd length, return result
+                    return maxLeft;
+                }
+
+                int minRight;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(A[i], B[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+}
