24559f4ea8d175e17e84c2c9439393268ddbb0d6
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-07-20 22:56:24 +0300

    Add problem 10
    Squashed commit of the following:
    
    commit 09733fa24db35aead5aa7d8ab78ccfb675967917
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Sat Jul 20 22:54:40 2019 +0300
    
        Complete problem
    
    commit d9247720d34c7ecda6b790423ad29332951e04be
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Sun Jun 16 00:12:47 2019 +0300
    
        Remove difficulty badge
    
    commit a231646b9ff786c444f2c3e4e43cbfcc95d51145
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Thu Jun 13 03:36:01 2019 +0300
    
        Add template

---
+++ Problem10/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    /**
+     * Time complexity: O(S * P) where S is length of string
+     *  and P is length of pattern.
+     *
+     * @param string String
+     * @param pattern String
+     * @return boolean
+     */
+    public boolean isMatch(String string, String pattern) {
+        boolean[][] DP = new boolean[string.length() + 1][pattern.length() + 1];
+        DP[string.length()][pattern.length()] = true;
+
+        for (int i = string.length(); -1 < i; i--) {
+            for (int j = pattern.length() - 1; -1 < j; j--) {
+                boolean match = i < string.length()
+                        && (pattern.charAt(j) == string.charAt(i)
+                        || pattern.charAt(j) == '.');
+
+                // check if pattern[j:] matches string[i:]
+                if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                    DP[i][j] = DP[i][j + 2] || match && DP[i + 1][j];
+                } else {
+                    DP[i][j] = match && DP[i + 1][j + 1];
+                }
+            }
+        }
+        return DP[0][0];
+    }
+}
