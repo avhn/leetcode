ffa8e332c2c7ef4b7088dc824cdb3ca984c09042
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-07-23 10:11:23 +0300

    Add problem 25
    Squashed commit of the following:
    
    commit edc979205b7cc87440da61b397b9a970731223b4
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Tue Jul 23 10:10:42 2019 +0300
    
        Complete problem
    
    commit ff570b7e8b9689ffedd62aab0da3b22bd83ff1fb
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Sun Jun 16 00:27:15 2019 +0300
    
        Add template

---
+++ Problem25/Solution.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    private ListNode reverse(ListNode start, ListNode end) {
+        ListNode head = end.next;
+        ListNode current = start;
+        ListNode next;
+
+        while (current != end) {
+            next = current.next;
+            current.next = head;
+            head = current;
+            current = next;
+        }
+
+        end.next = head;
+        return end;
+    }
+
+    /**
+     * Time complexity: O(n).
+     * Space complexity: O(1).
+     *
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     *
+     * @param head linked list
+     * @param k number of nodes to reverse
+     * @return new head of the linked list
+     * @see ListNode
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k < 1 || head == null) return null;
+        else if (k == 1) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode previous = dummy;
+        ListNode start;
+        ListNode end = dummy;   // initialize randomly to start the loop
+
+        while (end != null) {
+            start = previous.next;
+            end = start;
+
+            for (int i = 1; i < k && end != null; i++) {
+                end = end.next;
+            }
+
+            if (end != null) {
+                previous.next = reverse(start, end);
+                previous = start;   // start is the new end
+            }
+        }
+        return dummy.next;
+    }
+}
