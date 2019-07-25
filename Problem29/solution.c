11b670a40e1c3074111d61b30753211c92161622
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-07-25 15:56:07 +0300

    Add problem 29
    Squashed commit of the following:
    
    commit 04057a37411175a0f6f587c94efc798125cf862a
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Thu Jul 25 15:51:49 2019 +0300
    
        Complete problem
    
    commit 86e4d02d1a983e09f56a3881d32f80c290a29efc
    Author: Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
    Date:   Sun Jun 16 00:37:42 2019 +0300
    
        Add template

---
+++ Problem29/solution.c
@@ -0,0 +1,43 @@
+#include <limits.h>
+
+/**
+ * Helper function for divide.
+ */
+int unsignedDivision(unsigned int dividend, unsigned int divisor) {
+  if (dividend < divisor) return 0;
+  
+  unsigned int sum = divisor;
+  unsigned int quotient = 1;
+  // sum != INT_MAX + 1, is guard for uint overflow
+  while (sum != INT_MAX + 1 && sum + sum <= dividend) {
+    sum += sum;
+    quotient += quotient;
+    
+    /*
+     sum <<= 1;
+     quotient <<= 1;
+     */
+  }
+  
+  int diff = dividend - sum;
+  return quotient + unsignedDivision(diff, divisor);
+}
+
+/**
+ * Perform division of integers without using bigger sized datatypes
+ * and multiplication, division, mod operators.
+ *
+ * Time complexity: log(dividend).
+ */
+int divide(int dividend, int divisor){
+  if (divisor == 1) return dividend;
+  if (divisor == -1 && dividend == INT_MIN) return INT_MAX;
+  
+  unsigned int udividend = (dividend < 0) ? ((unsigned int) -(dividend + 1)) + 1 : dividend;
+  unsigned int udivisor = (divisor < 0) ? ((unsigned int) -(divisor + 1)) + 1 : divisor;
+  int result = unsignedDivision(udividend, udivisor);
+  int positive = (dividend < 0 && divisor < 0)
+    || (dividend > 0 && divisor > 0);
+  
+  return (positive) ? result : -result;
+}
