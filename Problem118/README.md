642ee12dc047aa45d33b86e3cc1cf2d8464a2f47
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-08-06 12:04:31 +0300

    Add problem 118

---
+++ Problem118/README.md
@@ -0,0 +1,20 @@
+ ## 118. Pascal's Triangle
+
+Given a non-negative integer *numRows*, generate the first *numRows* of Pascal's triangle.
+
+![pascal's triangle](../assets/problem118.gif)<br/>
+<sup>In Pascal's triangle, each number is the sum of the two numbers directly above it.</sup>
+
+**Example:**
+
+<pre>
+<b>Input:</b> 5
+<b>Output:</b>
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+</pre>
---
+++ Problem118/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    /**
+     * Generate rows for Pascal's triangle.
+     *
+     * Time complexity: O(n^2), where n is number of the rows.
+     * Space complexity: O(^2).
+     *
+     * @param numRows number of the rows to create
+     * @return generated rows as lists of a list
+     * @see List
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> currentList = new ArrayList();
+            for (int j = 0; j < i; j++) {
+                if (j == 0 || j == i - 1) {
+                    currentList.add(1);
+                } else {
+                    currentList.add(result.get(i - 2).get(j - 1)
+                            + result.get(i - 2).get(j));
+                }
+            }
+            result.add(currentList);
+        }
+        return result;
+    }
+}
