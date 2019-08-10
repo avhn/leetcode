d97a0d62548fb67f2dadd888dae32304984f2dea
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-08-11 01:26:17 +0300

    Add problem 189

---
+++ Problem189/README.md
@@ -0,0 +1,27 @@
+## 189. Rotate Array
+
+Given an array, rotate the array to the right by *k* steps, where *k* is non-negative.
+
+**Example 1:**
+<pre>
+<b>Input:</b> [1,2,3,4,5,6,7] and k = 3
+<b>Output:</b> [5,6,7,1,2,3,4]
+<b>Explanation:</b>
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+</pre>
+
+**Example 2:**
+<pre>
+<b>Input:</b> [-1,-100,3,99] and k = 2
+<b>Output:</b> [3,99,-1,-100]
+<b>Explanation:</b>
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+</pre>
+
+**Note:**
+
+- Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+- Could you do it in-place with O(1) extra space?
---
+++ Problem189/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    /**
+     * Rotate an array in-place to right.
+     *
+     * Time complexity: O(n), where n is length of the array.
+     * Space complexity: O(1).
+     *
+     * @param nums array
+     * @param k steps of the rotation
+     */
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int c = 0;  // count of swaps performed
+        int temp;   // swap element
+
+        for (int i = 0; c < len && i < k; i++) {
+            int j = i;
+            int carry = nums[i];
+            do {
+                j = (j + k) % len;
+                temp = nums[j];
+                nums[j] = carry;
+                carry = temp;
+                // i == j, loop overlapped, start again with i++
+                // ++c == len, all swaps performed, terminate
+            } while (++c < len && i != j);
+        }
+    }
+}
