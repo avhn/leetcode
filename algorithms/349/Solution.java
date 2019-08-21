633c2698d930ae469e7b67ce48af1eacea2250e3
Anaxilaus <24462323+Anaxilaus@users.noreply.github.com>
2019-08-21 14:15:53 +0300

    Update problem 349
    Improve space complexity

--- algorithms/349/Solution.java
+++ algorithms/349/Solution.java
@@ -3,13 +3,17 @@ import java.util.HashSet;
 class Solution {
 
     /**
-     * Time and space complexity: O(max(m, n)), where m and n are length of the arrays
+     * Time complexity: O(max(m, n))
+     * Space complexity: O(min(m, n))
+     * Where m and n are length of the arrays
      */
     public int[] intersection(int[] nums1, int[] nums2) {
-        if (nums1.length < nums2.length) return intersection(nums2, nums1);
+        // make nums1 smaller to reduce memory usage
+        if (nums1.length > nums2.length) return intersection(nums2, nums1);
 
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums1) set.add(num);
+        HashSet<Integer> set = new HashSet<Integer>() {{  // store nums1
+            for (int num : nums1) add(num);
+        }};
         HashSet<Integer> intersect = new HashSet<>();
         for (int num : nums2) {
             if (set.contains(num)) {
