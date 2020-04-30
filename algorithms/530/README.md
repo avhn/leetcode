1271a4f245ee76a6556bdf27c532f740385437b0
Anaxilaus <mertdede@protonmail.com>
2020-04-30 05:06:44 +0300

    Add problem 530

---
+++ algorithms/530/README.md
@@ -0,0 +1,24 @@
+## 530. Minimum Absolute Difference in BST
+
+Given a binary search tree with non-negative values, find the minimum [absolute difference](https://en.wikipedia.org/wiki/Absolute_difference) between values of any two nodes.
+
+**Example:**
+<pre>
+<b>Input:</b>
+
+   1
+         3
+    /
+   2
+
+<b>Output:</b>
+1
+
+<b>Explanation:</b>
+The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+</pre>
+
+**Note:**
+
+  * There are at least two nodes in this BST.
+  * This question is the same as 783: https://leetcode.com/problems/minimum-distance-between-bst-nodes/
---
+++ algorithms/530/solution.go
@@ -0,0 +1,49 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// Time complexity, O(n) where n is size of the tree
+// Space complexity, O(1)
+func getMinimumDifference(root *TreeNode) int {
+	diff := math.MaxInt64
+	var prev *TreeNode
+	calcMinDiff(root, &prev, &diff)
+	return diff
+}
+
+// Calculate min diff for a BST, traverse inorder recursively
+func calcMinDiff(root *TreeNode, prev **TreeNode, diff *int) {
+	if root != nil {
+		calcMinDiff(root.Left, prev, diff)
+		if *prev != nil {
+			*diff = min(*diff, abs(root.Val-(*prev).Val))
+		}
+		*prev = root
+		calcMinDiff(root.Right, prev, diff)
+	}
+}
+
+// Min util for int
+func min(n1 int, n2 int) int {
+	if n1 < n2 {
+		return n1
+	} else {
+		return n2
+	}
+}
+
+// Abs util for int
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
+}
