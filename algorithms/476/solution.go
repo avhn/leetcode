b2ba40898d1ef4ed01de14c1009a313feed5a249
Anaxilaus <mertdede@protonmail.com>
2020-05-01 20:15:37 +0300

    Update problem 476

--- algorithms/476/solution.go
+++ algorithms/476/solution.go
@@ -3,7 +3,7 @@ package main
 // Time and space complexity, O(1)
 func findComplement(num int) int {
 	var c int
-	for m := 1; num != 0; m, num = m*2, num>>1 {
+	for m := 1; num != 0; m, num = m<<1, num>>1 {
 		if num&1 != 1 {
 			c += m
 		}
