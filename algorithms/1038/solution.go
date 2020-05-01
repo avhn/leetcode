e2b2081d88fe34d6d9c76dd333859dce3d559aad
Anaxilaus <mertdede@protonmail.com>
2020-05-01 19:31:08 +0300

    Add problem 1038

---
+++ algorithms/1038/solution.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// Time complexity, O(n) where n is size of the tree
+// Space complexity, O(1)
+func bstToGst(root *TreeNode) *TreeNode {
+	var tempSum int
+	reverseInorder(root, &tempSum)
+	return root
+}
+
+// Form the greater tree recursively
+func reverseInorder(root *TreeNode, sum *int) {
+	if root == nil {
+		return
+	}
+	reverseInorder(root.Right, sum)
+	*sum += root.Val
+	root.Val = *sum
+	reverseInorder(root.Left, sum)
+}
